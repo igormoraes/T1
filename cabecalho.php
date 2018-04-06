19ad5cd20ec94ff14e7bdc61eb46ca235d0817b8
igormoraes <moralizandoTv@gmail.com>
2018-04-06 15:37:54 -0300

    Att v3

---
+++ cabecalho.php
@@ -0,0 +1,6 @@
+<a href="a.php"> A |</a>
+<a href="a.php"> B |</a>
+<a href="a.php"> C |</a>
+<a href="a.php"> D |</a>
+<a href="a.php"> E |</a>
+<a href="a.php"> F |</a>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title> Pagina Inicial </title>
+	</head>
+	<body>
+		<?php 
+			include "cabecalho.php";
+		?>
+	</body>
+</html>
