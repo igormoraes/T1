0cc4c078f2b901c3242cbaca1aea22040884563d
igormoraes <moralizandoTv@gmail.com>
2018-04-11 17:24:09 -0300

    Atualizações no index.php e na tabela.php

--- cabecalho.php
+++ cabecalho.php
@@ -1,3 +1,5 @@
-<a href="index.php"> Pagina Inicial |</a>
-<a href="cadastra_estado.php"> Cadastrar Estado |</a>
-<a href="cadastra_cidade.php"> Cadatrar Cidade |</a>
+<header>
+	<a href="index.php"> Pagina Inicial |</a>
+	<a href="cadastra_estado.php"> Cadastrar Estado |</a>
+	<a href="cadastra_cidade.php"> Cadatrar Cidade |</a>
+</header>
--- tabelas.php
+++ tabelas.php
@@ -1,42 +1,53 @@
-<!--Tabela Estados-->
-<table border="1">
-	<th>Estados Cadastrados</th>
-	<tr>
-		<td>linha 1, célula 1</td>
-		
-	</tr>
-	<tr>
-		<td>linha 1, célula 1</td>
-		
-	</tr>
-</table>
-<br />
+<?php
 
-<!--Tabela Cidades-->
-<table border="1">
-	<th>Cidades Cadastrados</th>
-	<tr>
-		<td>linha 1, célula 1</td>
-		
-	</tr>
-	<tr>
-		<td>linha 1, célula 1</td>
-		
-	</tr>
-</table>
-<br />
+//Tabela Estados
 
-<!--Tabela Cadastros-->
-<table border="1">
-	<th>Cadastrados Usuarios</th>
-	<tr>
-		<td>linha 1, célula 1</td>
-		
-	</tr>
-	<tr>
-		<td>linha 1, célula 1</td>
-		
-	</tr>
-</table>
-<br />
+echo '
 
+	<table border="1">
+		<th>Estados Cadastrados</th>
+		<tr>
+			<td>linha 1, célula 1</td>
+		
+		</tr>
+		<tr>
+			<td>linha 1, célula 1</td>
+		
+		</tr>
+	</table>
+	<br />';
+
+//Tabela Cidades
+
+echo'
+
+	<table border="1">
+		<th>Cidades Cadastrados</th>
+		<tr>
+			<td>linha 1, célula 1</td>
+		
+		</tr>
+		<tr>
+			<td>linha 1, célula 1</td>
+		
+		</tr>
+	</table>
+	<br />';
+
+//Tabela Cadastros
+
+echo'
+
+	<table border="1">
+		<th>Cadastrados Usuarios</th>
+		<tr>
+			<td>linha 1, célula 1</td>
+		
+		</tr>
+		<tr>
+			<td>linha 1, célula 1</td>
+		
+		</tr>
+	</table>
+	<br />';
+?>
