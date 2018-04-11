1939cfe1a86791ef129af60891ee32ee7289e471
igormoraes <moralizandoTv@gmail.com>
2018-04-11 17:42:32 -0300

    Atualização cabecalho.php

--- cabecalho.php
+++ cabecalho.php
@@ -2,4 +2,5 @@
 	<a href="index.php"> Pagina Inicial |</a>
 	<a href="cadastra_estado.php"> Cadastrar Estado |</a>
 	<a href="cadastra_cidade.php"> Cadatrar Cidade |</a>
+	<a href="form_cadastro.php"> Cadatrar Pessoa |</a>
 </header>
